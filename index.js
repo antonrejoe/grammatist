a3ae79f958fd3fe4243bf96c9297dc536d610b3a
anton_rejoe <antonrejoe@gmail.com>
2022-08-16 15:13:17 +0530

    site getting responsive lol

--- index.js
+++ index.js
@@ -1,4 +1,15 @@
 
 
-//// Dark mode
 
+
+//changing the links color on hovering
+
+
+$(".dropconent_2").hover(function () {
+    $(this).css("background-color", "#54BAB9");
+        
+    }, function () {
+        $(this).css("background-color","#9FD3C7")
+        // out
+    }
+);  
